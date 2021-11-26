9c57ebc0f1db353ddcf939c312ae9652d8e4dd83
Oğuzhan Demir <74780105+OzyyDemir@users.noreply.github.com>
2021-11-26 16:00:53 +0300

    Delete myApp/myApp directory

--- myApp/myApp/src/main/java/com/myApp/myApp/dataAccess/abstracts/OpinionDao.java
+++
@@ -1,9 +0,0 @@
-package com.myApp.myApp.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.myApp.myApp.entities.concretes.Opinion;
-
-public interface OpinionDao extends JpaRepository<Opinion,Integer>{
-
-}
--- myApp/myApp/src/main/java/com/myApp/myApp/entities/concretes/UpdateUserVM.java
+++
@@ -1,9 +0,0 @@
-package com.myApp.myApp.entities.concretes;
-
-import lombok.Data;
-
-@Data
-public class UpdateUserVM {
-
-    private String userFirstName;
-}
